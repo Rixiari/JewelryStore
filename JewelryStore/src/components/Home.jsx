32f90cad8c95646c8cc14d5fd30e41c75d2cf989
Rixiari <rixiari@LAPTOP-E4RKFESK>
2024-03-02 23:15:51 -0500

    home page draft

---
+++ JewelryStore/src/components/Home.jsx
@@ -0,0 +1,31 @@
+import homeimg1 from '../assets/homeimg/P71.2.jpg'
+import homeimg2 from '../assets/homeimg/P129.jpg'
+import homeimg3 from '../assets/homeimg/P135.2.jpg'
+import homeimg4 from '../assets/homeimg/P142.jpg'
+import homeimg5 from '../assets/homeimg/P174.2.jpg'
+import homeimg6 from '../assets/homeimg/P176.3.jpg'
+import homeimg7 from '../assets/homeimg/P200.2.jpg'
+import homeimg8 from '../assets/homeimg/P236.3.jpg'
+import homeimg9 from '../assets/homeimg/P276.2.jpg'
+
+function Home() {
+
+  return (
+<div>
+<h1>House of Heera</h1>
+<p>Elegant and timeless designs crafted with exquisite attention to detail, epitomizing sophistication and opulence for the discerning connoisseur of luxury.</p>
+<img src={homeimg1}/>
+<img src={homeimg2}/>
+<img src={homeimg3}/>
+<img src={homeimg4}/>
+<img src={homeimg5}/>
+<img src={homeimg6}/>
+<img src={homeimg7}/>
+<img src={homeimg8}/>
+<img src={homeimg9}/>
+</div>
+
+  )
+}
+
+export default Home;
